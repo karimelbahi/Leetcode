6041a8cbbc50c00d95bed1b1ea930d71300ad76d
karimelbahi <karimmaster333@gmail>
2021-09-06 21:00:58 +0200

    3th pb is palindrome

---
+++ src/IsPalindrome.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println(isPalindrome(121))
+}
+
+fun isPalindrome(num: Int): Boolean {
+    if (num.toLong() < 0) return false
+
+    var numString = num.toString()
+    return numString.equals(numString.reversed())
+
+}

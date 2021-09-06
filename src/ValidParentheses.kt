b7c2c9b60fa794b465ca3e1cff49350c4c1c4407
karimelbahi <karimmaster333@gmail>
2021-09-06 21:41:13 +0200

    6th pb Valid Parentheses

---
+++ src/ValidParentheses.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+   println(isValid("()"))
+   println(isValid("()[]{}"))
+   println(isValid("(]"))
+   println(isValid("([)]"))
+   println(isValid("{[]}"))
+
+}
+
+fun isValid(s: String): Boolean {
+    if ((s.length <= 1)) return false
+    var stack: Stack<Char> = Stack()
+    for (i in s.withIndex()) {
+        when (i.value) {
+            '(' -> {
+                stack.push(')')
+            }
+            '{' -> {
+                stack.push('}')
+            }
+            '[' -> {
+                stack.push(']')
+            }
+            ')', ']', '}' -> {
+                if (stack.isEmpty() || stack.pop() != i.value) return false
+            }
+        }
+    }
+    return stack.isEmpty()
+}

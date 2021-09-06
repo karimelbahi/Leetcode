be6b9ffdc2e5a7ca927ba449e17b75e3bd4a960b
karimelbahi <karimmaster333@gmail>
2021-09-06 20:59:15 +0200

    2th pb reverse integer

---
+++ src/ReverseInteger.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    println(reverse(1234))
+}
+fun reverse(num: Int): Int {
+    var numString = if (num.toLong() < 0) (num.toLong() * -1).toString() else num.toLong().toString()
+
+    numString = numString.reversed()
+    return if (num < 0 && numString.toLong() * -1 > Integer.MIN_VALUE)
+        numString.toInt() * -1
+    else if (num > 0 && numString.toLong() < Integer.MAX_VALUE)
+        numString.toInt()
+    else 0
+
+}

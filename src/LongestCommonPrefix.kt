552b3fc8b2422de90eadf12518c4c15340d4c58c
karimelbahi <karimmaster333@gmail>
2021-09-06 21:06:51 +0200

    4th pb roman to int

---
+++ src/LongestCommonPrefix.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    println(longestCommonPrefix(arrayOf("flower", "flow", "flight")))
+    println(longestCommonPrefix(arrayOf("doge", "racecar", "car")))
+}
+
+fun longestCommonPrefix(strs: Array<String>): String {
+
+    var prefix = strs[0]
+    for (i in 1 until strs.size) {
+        prefix = prefix.commonPrefixWith(strs[i])
+    }
+
+    return prefix;
+}

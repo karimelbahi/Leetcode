c4dc7de1fabaa00575243960747cfe49aff3def8
karimelbahi <karimmaster333@gmail>
2021-09-06 20:50:57 +0200

    first problem two sum

---
+++ src/TwoSum.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    var lst = intArrayOf(3, 3, 2, 5, 9)
+    twoSum(lst, 11)
+}
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+
+    var numMap: MutableMap<Int, Int> = HashMap()
+
+    for (indexOne in nums.withIndex()) {
+        var complement = target - indexOne.value
+        if (numMap.containsKey(complement)) {
+            return intArrayOf(numMap[complement]!!, indexOne.index)
+        }
+        numMap[nums[indexOne.index]] = indexOne.index
+    }
+    return intArrayOf(0)
+}

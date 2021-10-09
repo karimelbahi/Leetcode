f3574392a1b1984c1f9e17fd0573372e160b3e72
karimelbahi <karimmaster333@gmail>
2021-10-09 21:39:24 +0200

    27. Remove Element

---
+++ src/RemoveElement.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println(removeElement(intArrayOf(3, 2, 2, 3), 3))
+    println(removeElement(intArrayOf(0, 1, 2, 2, 3, 0, 4, 2), 2))
+}
+
+fun removeElement(nums: IntArray, num: Int): Int {
+
+    var pointer = 0
+    for (index in nums.indices) {
+        nums[index - pointer] = nums[index]
+        if (nums[index] == num) {
+            pointer++
+        }
+    }
+    return nums.size - pointer
+
+}
